4235064fd343f8df5a0003210fa984b5fc78b164
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-06 12:56:02 +0530

    Reattempted number of good pairs in normal double loop

--- src/Array/LeetCodeQue/NumberOfGoodPairs.java
+++ src/Array/LeetCodeQue/NumberOfGoodPairs.java
@@ -7,9 +7,9 @@ public class NumberOfGoodPairs {
     }
 
 
-    static  int numIdenticalPairs(int[] nums) {
-        int count = 0;
-        for(int i = 0; i < nums.length-1; i++){
+    public static int numIdenticalPairs(int[] nums) {
+        int count  = 0;
+        for(int i =0; i < nums.length-1; i++){
             for(int j = i+1; j < nums.length; j++){
                 if(nums[i] == nums[j]){
                     count++;
