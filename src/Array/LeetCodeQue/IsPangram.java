bd19a43346a1a485acb9dfb99f851c276f153a36
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-10 20:41:43 +0530

    Did isPangram

---
+++ src/Array/LeetCodeQue/IsPangram.java
@@ -0,0 +1,23 @@
+package Array.LeetCodeQue;
+
+//  https://leetcode.com/problems/check-if-the-sentence-is-pangram/
+public class IsPangram {
+    public static void main(String[] args) {
+        String sentence = "thequickbrownfoxjumpsoverthelazydog";
+        System.out.println(checkIfPangram(sentence));
+    }
+
+    static boolean checkIfPangram(String sentence) {
+        boolean[] alphabet = new boolean[26];
+        char[] alpha = sentence.toCharArray();
+
+        for(char ch: alpha){
+            alphabet[ch - 'a'] = true;
+        }
+
+        for(boolean isAlpha : alphabet){
+            if(!isAlpha) return false;
+        }
+        return true;
+    }
+}
