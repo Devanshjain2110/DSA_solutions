edf382bb1f5c6f9eb1086aa8743d6b27317a1b4f
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-11 21:56:45 +0530

    Did leetcode medium with maximum optimzation

--- src/Array/LeetCodeQue/ProductOfArrayExceptSelf.java
+++ src/Array/LeetCodeQue/ProductOfArrayExceptSelf.java
@@ -1,38 +1,59 @@
 package Array.LeetCodeQue;
 
+import java.util.Arrays;
+
 public class ProductOfArrayExceptSelf {
     public static void main(String[] args) {
         int[] arr = {1,2,3,4};
-        productExceptSelf(arr);
+        System.out.println(Arrays.toString(productExceptSelf1(arr)));
     }
-    public static int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] suffix = new int[nums.length];
-        int[] answer = new int[nums.length];
-        int p = 1;
 
-        for(int i = 0; i < nums.length; i++){
-            prefix[i] = nums[i]*p;
-            p = prefix[i];
-        }
-        int ps = 1;
-        for(int j = nums.length-1 ; j >= 0 ; j--){
-            suffix[j] = nums[j]*ps;
-            ps = suffix[j];
+    // O(1) Space
+
+    static int[] productExceptSelf1(int[] nums) {
+        int n = nums.length;
+        int[] arr = new int[n];
+
+        int productBefore = 1;
+        for(int i = 0; i < n; i++){
+            arr[i] = productBefore;
+            productBefore *= nums[i];
         }
 
-        for(int i = 0; i < nums.length; i++){
-            if(i == 0){
-                answer[i] = suffix[i+1];
-            }
-            else if(i == nums.length-1){
-                answer[i] = prefix[i-1];
-            }
-            else{
-                answer[i] = prefix[i-1] * suffix[i+1];
-            }
+
+        int productAfter = 1;
+        for(int i = n-1; i >= 0; i--){
+            arr[i] *= productAfter;
+            productAfter *= nums[i];
         }
-        return answer;
+
+
+        return arr;
+    }
+
+    // O(N) space
+    static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] prefixArray = new int[n];
+        int[] suffixArray = new int[n];
+
+        prefixArray[0] = nums[0];
+        for(int i = 1; i < n; i++){
+            prefixArray[i] = prefixArray[i-1] * nums[i];
+        }
+
+
+        suffixArray[n-1] = nums[n-1];
+        for(int i = n - 2; i >= 0 ; i--){
+            suffixArray[i] = suffixArray[i+1] * nums[i];
+        }
+
+        nums[0] = suffixArray[1];
+        nums[n-1] = prefixArray[n-2];
+        for(int i = 1; i < n-1; i++){
+            nums[i] = prefixArray[i-1] * suffixArray[i+1];
+        }
+        return nums;
     }
 }
 
