d320d9428fd6bcdd5492bac326a260d413a2b7c3
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-20 13:13:33 +0530

    Did tranpose of matrix

--- src/Array/LeetCodeQue/TransposeOfMatrix.java
+++ src/Array/LeetCodeQue/TransposeOfMatrix.java
@@ -1,18 +1,25 @@
 package Array.LeetCodeQue;
+
+import java.util.Arrays;
+
 // https://leetcode.com/problems/transpose-matrix/
 public class TransposeOfMatrix {
     public static void main(String[] args) {
-
+int[][] mat = {{1,2,3},{4,5,6},{7,8,9}};
+        System.out.println(Arrays.deepToString(transpose(mat)));
     }
-  static int[][] transpose(int[][] matrix) {
-        int n = matrix.length, m = matrix[0].length;
-        int[][] transposedMatrix = new int[m][n];
+    static int[][] transpose(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] res = new int[cols][rows];
 
-        for (int i=0; i<m; i++)
-            for (int j=0; j<n; j++)
-                transposedMatrix[i][j] = matrix[j][i];
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<cols;j++){
+                res[j][i]=matrix[i][j];
+            }
+        }
 
-        return transposedMatrix;
+        return res;
     }
-
 }
+
