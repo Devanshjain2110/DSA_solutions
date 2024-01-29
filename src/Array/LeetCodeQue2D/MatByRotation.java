9b5f457c9fa8b9d836173df95f1652225a0fdc0d
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-01-29 23:50:21 +0530

    Did matrix rotattion

---
+++ src/Array/LeetCodeQue2D/MatByRotation.java
@@ -0,0 +1,28 @@
+package Array.LeetCodeQue2D;
+
+// https://leetcode.com/problems/determine-whether-matrix-can-be-obtained-by-rotation/
+public class MatByRotation {
+    public static void main(String[] args) {
+
+    }
+  static boolean findRotation(int[][] mat, int[][] target) {
+        int Rot90=0,Rot180=0,Rot270=0,Rot0 = 0;
+        int len = target.length;
+
+        for(int i = 0; i < len; i++) {
+            for(int j = 0; j < len; j++){
+
+                if(target[i][j] == mat[len-j-1][i]) Rot90++;
+
+                if(target[i][j] == mat[len-i-1][len-j-1]) Rot180++;
+
+                if(target[i][j] == mat[j][len-i-1]) Rot270++;
+
+                if(target[i][j] == mat[i][j]) Rot0++;
+            }
+        }
+
+        return ( Rot90 == len*len || Rot180 == len*len || Rot270 == len*len || Rot0 == len*len );
+
+    }
+}
