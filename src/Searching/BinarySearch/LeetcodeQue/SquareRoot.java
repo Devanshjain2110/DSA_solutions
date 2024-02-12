2a6d69c2c4ac482c16d0e18ad51324ff34713201
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-12 16:58:22 +0530

    Did square root of matrix

---
+++ src/Searching/BinarySearch/LeetcodeQue/SquareRoot.java
@@ -0,0 +1,23 @@
+package Searching.BinarySearch.LeetcodeQue;
+// https://leetcode.com/problems/sqrtx/
+public class SquareRoot {
+    public static void main(String[] args) {
+        System.out.println(mySqrt(99));
+    }
+    static int mySqrt(int x) {
+        int start = 0;
+        int end = x;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+
+            if((long) mid*mid > (long) x){
+                end = mid-1;
+            } else if(mid*mid < x){
+                start = mid+1;
+            } else{
+                return mid;
+            }
+        }
+        return end;
+    }
+}
