d9fdc4d78b624f4ce764a57cb131da4bcac8dd27
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-14 19:04:48 +0530

    Did first bad version using binary search

---
+++ src/Searching/BinarySearch/LeetcodeQue/FirstBadVersion.java
@@ -0,0 +1,25 @@
+package Searching.BinarySearch.LeetcodeQue;
+//  https://leetcode.com/problems/first-bad-version/
+public class FirstBadVersion {
+    public static void main(String[] args) {
+
+    }
+//isBadVersion API checks for is bad version
+//    static int firstBadVersion(int n) {
+//        int start = 1;
+//        int end = n;
+//        if(n == 1) {
+//            if(isBadVersion(1)) return 1;
+//        }
+//        while(start <= end) {
+//            int mid = start + (end-start)/2;
+//
+//            if(isBadVersion(mid)){
+//                end = mid-1;
+//            }else {
+//                start = mid+1;
+//            }
+//        }
+//        return start;
+//    }
+}
