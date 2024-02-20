78250192263a34f4903852cd936aed72192b2630
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-20 12:52:04 +0530

    Did leetcode 1539

---
+++ src/Searching/BinarySearch/LeetcodeQue/FindKthPosition.java
@@ -0,0 +1,27 @@
+package Searching.BinarySearch.LeetcodeQue;
+
+// https://leetcode.com/problems/kth-missing-positive-number/
+public class FindKthPosition {
+    public static void main(String[] args) {
+       int[] arr = {2,3,4,7,11};
+        int k = 5;
+        System.out.println( findKthPositive(arr, k));
+    }
+    static int findKthPositive(int[] nums, int k) {
+        int start = 0;
+        int end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[mid] - mid <= k) {
+                start = mid + 1;
+            }
+            else {
+                end = mid - 1;
+            }
+        }
+
+        return start + k;
+    }
+}
