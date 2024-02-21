33b63a2f3bad1fadd6923b659332211d71ea2950
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-21 09:37:47 +0530

    Did leetcode 852 peak index in mountain array

--- src/Searching/BinarySearch/LeetcodeQue/FindPeakElement.java
+++ src/Searching/BinarySearch/LeetcodeQue/FindPeakElement.java
@@ -1,19 +1,22 @@
 package Searching.BinarySearch.LeetcodeQue;
-
+// https://leetcode.com/problems/peak-index-in-a-mountain-array/
 public class FindPeakElement {
-    static int findPeakElement(int[] nums) {
-
+    public static void main(String[] args) {
+        int[] arr = {0,2,1,0};
+        System.out.println(peakIndexInMountainArray(arr));
+    }
+   static int peakIndexInMountainArray(int[] arr) {
         int start = 0;
-        int end = nums.length-1;
-
-        while(start<end){
+        int end = arr.length-1;
+        while(start < end){
             int mid = start + (end-start)/2;
-            if(nums[mid] < nums[mid + 1]) {
-                start = mid + 1;
-            } else {
+            if(arr[mid] > arr[mid+1]){
                 end = mid;
+            } else {
+                start = mid+1;
             }
         }
         return start;
-    }}
+    }
+}
 
