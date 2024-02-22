7cfddcc4f510a8586e140079c86b28bdc7efb656
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-22 13:06:11 +0530

    Did leetcode 1351 in one go

---
+++ src/Searching/BinarySearch2D/LeetcodeQue/CountNegativeNumberInSortedMatrix.java
@@ -0,0 +1,24 @@
+package Searching.BinarySearch2D.LeetcodeQue;
+// https://leetcode.com/problems/count-negative-numbers-in-a-sorted-matrix/
+public class CountNegativeNumberInSortedMatrix {
+    public static void main(String[] args) {
+        int[][] grid = {{4,3,2,-1},{3,2,1,-1},{1,1,-1,-2},{-1,-1,-2,-3}};
+        System.out.println(countNegatives(grid));
+    }
+    static int countNegatives(int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+        int row = 0;
+        int col = grid[0].length-1;
+        int count = 0;
+        while(row < rows && col >= 0){
+            if(grid[row][col] >= 0){
+                row++;
+            } else {
+                count += rows-row;
+                col--;
+            }
+        }
+        return count;
+    }
+}
