816676daa9eb5c62d13450d1c5898f79f5aa791a
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-02-24 17:58:01 +0530

    Did first and last position with 100% accuracy

--- src/Searching/BinarySearch/LeetcodeQue/FirstAndLastPosition.java
+++ src/Searching/BinarySearch/LeetcodeQue/FirstAndLastPosition.java
@@ -1,72 +1,45 @@
 package Searching.BinarySearch.LeetcodeQue;
-
+// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 import java.util.Arrays;
 
 public class FirstAndLastPosition {
-// https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
-    static int[] ans(int[] nums, int target ){
-        int[] ans = {-1, -1};
 
-        int start = search(nums, target, true);
-        int end = search(nums, target, false);
 
-        ans[0] = start;
-        ans[1] = end;
-        return ans;
-    }
-
-    static int search(int[] arr, int target, boolean isStart){
-        int ans = -1;
-        int start = 0;
-        int end  = arr.length -1;
-
-        while(start<=end){
-            int mid = start + (end-start)/2;
-
-            if(target < arr[mid]){
-                end = mid -1;
-            }
-            else if(target > arr[mid]){
-                start = mid + 1;
-            }
-            else{
-                ans = mid;
-                if(isStart){
-                          end = mid -1;
-                }else{
-                    start = mid + 1;
-                }
-            }
-
-        }
-        return ans;
-    }
-
-    // Brute force
-    public int[] searchRange(int[] nums, int target) {
-        if(nums.length == 0) return new int[] {-1,-1};
-        int before = Integer.MIN_VALUE;
-        int after = Integer.MIN_VALUE;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                if(before > Integer.MIN_VALUE){
-                    after = i;
-                } else {
-                    before = i;
-                }
-
-            }
-        }
-        if(before != Integer.MIN_VALUE && after == Integer.MIN_VALUE) return new int[] {before, before};
-        if(before == Integer.MIN_VALUE ) return new int[] {-1,-1};
-
-        return new int[]{before, after};
-    }
 
     public static void main(String[] args) {
-        int[] nums = {2,3,4,7,7,7,7,8,9};
-        int target = 7;
-        String x = Arrays.toString(ans(nums, target));
+        int[] nums = {5,7,7,8,8,10};
+        int target = 8;
+        String x = Arrays.toString(searchRange(nums, target));
         System.out.println(x);
+    }
+
+    static int[] searchRange(int[] nums, int target) {
+
+        int first = binarySearch(nums, 0, nums.length-1, false,target);
+        int end = binarySearch(nums, 0, nums.length-1, true, target);
+
+        return new int[] {first, end};
+    }
+
+    static int binarySearch(int[] arr,int start,int end, boolean isLast, int target){
+
+        int ans = -1;
+        while(start <= end){
+            int mid = start + (end-start)/2;
+            if(arr[mid] > target){
+                end = mid-1;
+            } else if(arr[mid] < target){
+                start = mid+1;
+            } else {
+                ans = mid;
+                if(isLast){
+                    start = mid + 1;
+                } else {
+                    end = mid-1;
+                }
+            }
+
+        }
+        return ans;
     }
 }
