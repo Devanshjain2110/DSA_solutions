6671392768c7a89db1ae77291fd97d3bc2ac9af4
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-03-20 10:09:12 +0530

    DId rotated binary search 2

--- src/Searching/BinarySearch/LeetcodeQue/RotatedBinarySearch.java
+++ src/Searching/BinarySearch/LeetcodeQue/RotatedBinarySearch.java
@@ -3,64 +3,59 @@ package Searching.BinarySearch.LeetcodeQue;
 public class RotatedBinarySearch {
     public static void main(String[] args) {
         int[] arr = {4,5,6,7,0,1,2};
-        System.out.println(findElement(arr,5));
+        System.out.println(search(arr,5));
     }
 
-    static int findElement(int[] nums, int target){
+    static int search(int[] nums, int target) {
         int pivot = findPivot(nums);
+        if(pivot == -1) return binarySearch(nums, target, 0, nums.length-1);
+        if(nums[pivot] == target) return pivot;
 
-        if(pivot == -1){
-            search(nums, target,0 ,nums.length-1);
+
+
+        if(target >= nums[0]){
+            return binarySearch(nums, target, 0, pivot);
         }
 
-        if(nums[pivot] == target){
-            return pivot;
-        }
-
-        if(nums[0] >= target){
-            return search(nums,target, pivot+1, nums.length-1);
-        }
-            return search(nums,target,0,pivot-1);
-    }   
+        return binarySearch(nums, target, pivot+1, nums.length-1);
+    }
 
 
-    static int findPivot(int[] nums){
-        int start = 0;
-        int end = nums.length-1;
+    static int binarySearch(int[] arr, int target, int start, int end){
+        while(start <=end){
+            int mid = start + (end-start)/2;
 
-        while(start<=end){
-            int mid = start +(end-start)/2;
-
-            if(end > mid  && nums[mid] > nums[mid + 1]){
+            if(arr[mid] > target){
+                end = mid-1;
+            } else if(arr[mid] < target){
+                start = mid+1;
+            } else {
                 return mid;
-            }
-            if(mid > start && nums[mid-1] > nums[mid]){
-                return mid-1;
-            }
-
-            if(nums[start] >= nums[mid]) {
-                end = mid - 1;
-            }else{
-                start = mid + 1;
             }
         }
         return -1;
     }
 
-    static int search(int[] arr, int target, int start, int end){
-        while(start<=end){
+    static int findPivot(int[] arr){
+        int start = 0;
+        int end = arr.length-1;
+
+        while(start <= end){
             int mid = start + (end-start)/2;
 
-            if(arr[mid] >target){
-                end=mid-1;
-            }
-            else if(arr[mid] < target){
-                start = mid+1;
-            }
-            else {
+            if(mid <  end && arr[mid] > arr[mid+1]){
                 return mid;
             }
 
+            if(start < mid && arr[mid -1] > arr[mid]){
+                return mid-1;
+            }
+
+            if(arr[start] >= arr[mid]){
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
         }
         return -1;
     }
---
+++ src/Searching/BinarySearch/LeetcodeQue/SearchInRotatedSortedArray2.java
@@ -0,0 +1,40 @@
+package Searching.BinarySearch.LeetcodeQue;
+ // https://leetcode.com/problems/search-in-rotated-sorted-array-ii/
+public class SearchInRotatedSortedArray2 {
+    public static void main(String[] args) {
+int[] nums = {1,0,1,1,1};
+       int target = 0;
+    }
+    public boolean search(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) return true;
+
+            //if there are duplicates
+            if(nums[start] == nums[mid] && nums[mid] == nums[end]) {
+                start ++;
+                end --;
+            }
+
+            //left half is sorted
+            else if(nums[start] <= nums[mid]) {
+                if(target >= nums[start] && target <= nums[mid])
+                    end = mid - 1;
+
+                else
+                    start = mid + 1;
+            }
+
+            //right half is sorted
+            else {
+                if(target <= nums[end] && target >= nums[mid])
+                    start = mid + 1;
+
+                else
+                    end = mid - 1;
+            }
+        }
+        return false;
+    }
+}
