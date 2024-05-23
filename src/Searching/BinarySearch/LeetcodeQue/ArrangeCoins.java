63af321a3eb0a8ad7008359aa6a38b46c29d9ca3
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-05-23 09:08:50 +0530

    Did arrange coins

--- src/Searching/BinarySearch/LeetcodeQue/ArrangeCoins.java
+++ src/Searching/BinarySearch/LeetcodeQue/ArrangeCoins.java
@@ -9,10 +9,10 @@ public class ArrangeCoins {
         long end = n;
         while(start<=end){
             long mid = start + (end-start)/2;
-            long sum = mid*(mid+1)/2;
-            if(sum == n){
+            long sm = mid*(mid+1)/2;
+            if(sm == n){
                 return (int)mid;
-            }else if(sum > n){
+            }else if(sm > n){
                 end = mid-1;
             } else {
                 start = mid+1;
