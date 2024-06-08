4075139ee07948e1b63b0fa62c66756bb85d1721
Devanshjain2110 <92254216+Devanshjain2110@users.noreply.github.com>
2024-06-08 08:02:33 +0530

    Did peak element

---
+++ src/Searching/BinarySearch/LeetcodeQue/PeakElement.java
@@ -0,0 +1,20 @@
+package Searching.BinarySearch.LeetcodeQue;
+// https://leetcode.com/problems/find-peak-element/
+public class PeakElement {
+    public static void main(String[] args) {
+
+    }
+    static int findPeakElement(int[] nums) {
+        int start = 0;
+        int end = nums.length-1;
+        while(start < end){
+            int mid = start + (end-start)/2;
+            if(nums[mid] < nums[mid+1]){
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
+    }
+}
